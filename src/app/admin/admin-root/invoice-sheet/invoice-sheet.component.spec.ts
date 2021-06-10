2447a041ef6d45b329d8ee5452f105876df735f4
Rizka Indah Puspita <rizkaindahpuspita@gmail.com>
2021-06-10 13:43:23 +0700

    invoice design

---
+++ src/app/admin/admin-root/invoice-sheet/invoice-sheet.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InvoiceSheetComponent } from './invoice-sheet.component';
+
+describe('InvoiceSheetComponent', () => {
+  let component: InvoiceSheetComponent;
+  let fixture: ComponentFixture<InvoiceSheetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InvoiceSheetComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InvoiceSheetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin/admin-root/invoice-sheet/invoice-sheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-sheet',
+  templateUrl: './invoice-sheet.component.html',
+  styleUrls: ['./invoice-sheet.component.css']
+})
+export class InvoiceSheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
